af1a3d341040cb6eba754bb33b26b06f5af8728f
kezaqueen <rutagengwakezaqueen@gmail.com>
2025-06-21 11:06:00 +0300

    Revert "Feature/design review"

--- mosaic/design_review/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-# from models import design_review
-# Register your models here.
-
-
-# admin.site.register(design_review)
--- mosaic/shopping_cart/models.py
+++
@@ -1,9 +0,0 @@
-# from django.db import models
-
-# # Create your models here.
-# class Shopping_cart(models.Model):
-#     trader = models.ForeignKey(trader_details, on_delete= models.CASCADE)
-#     quantity = models.IntegerField()
-#     price = models.DecimalField()
-
-
