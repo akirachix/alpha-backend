4ca494de25af7e05c66de0f502b7c8f455ecf046
mildredosebe <mildredosebeochoki@gmail.com>
2025-06-24 15:19:32 +0300

    created apis

--- mosaic/api/urls.py
+++ mosaic/api/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path ,include
 from rest_framework.routers import DefaultRouter
-from.views import DesignViewSet
+from.views import (DesignViewSet,DesignersViewSet,TraderViewSet,OrderViewSet,PaymentViewSet,Shopping_cartViewSet,Transaction,design_review)
 router=DefaultRouter()
-router.register(r"products",DesignViewSet,basename="product")
+router.register(r"design",DesignViewSet,basename="design")
+router.register(r"designers",DesignViewSet,basename="designers")
+router.register(r"trader",DesignViewSet,basename="trader")
+router.register(r"order",DesignViewSet,basename="order")
+router.register(r"payment",DesignViewSet,basename="payment")
+router.register(r"shopping_cart",DesignViewSet,basename="shopping_cart")
+router.register(r"transaction",DesignViewSet,basename="transaction")
+router.register(r"design_review",DesignViewSet,basename="design_review")
+
+
 urlpatterns=[path("",include(router.urls)),]
--- mosaic/api/views.py
+++ mosaic/api/views.py
@@ -3,7 +3,43 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework import viewsets
 from catalogue.models import Design
-from .serializers import DesignSerializer
+from designers.models import Designers
+from design_review.models import design_review
+from order.models import Order
+from payment.models import Payment
+from shopping_cart.models import Shopping_cart
+from traders.models import Trader
+from transaction.models import Transaction
+
+from .serializers import (DesignSerializer,DesignersSerializer,TraderSerializer,OrderSerializer,PaymentSerializer,Shopping_cartSerializer,TransactionSerializer,design_reviewSerializer)
+
+
 class DesignViewSet(viewsets.ModelViewSet):
     queryset=Design.objects.all()
     serializer_class=DesignSerializer
+class DesignersViewSet(viewsets.ModelViewSet):
+    queryset=Designers.objects.all()
+    serializer_class=DesignersSerializer
+class TraderViewSet(viewsets.ModelViewSet):
+    queryset=Trader.objects.all()
+    serializer_class= TraderSerializer
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset=Order.objects.all()
+    serializer_class= OrderSerializer
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset=Payment.objects.all()
+    serializer_class= PaymentSerializer
+class Shopping_cartViewSet(viewsets.ModelViewSet):
+    queryset=Shopping_cart.objects.all()
+    serializer_class=Shopping_cartSerializer
+class Transaction(viewsets.ModelViewSet):
+    queryset=Transaction.objects.all()
+    serializer_class=TransactionSerializer
+class design_review(viewsets.ModelViewSet):
+    queryset=design_review.objects.all()
+    serializer_class=design_reviewSerializer
+
+
+
+  
+
