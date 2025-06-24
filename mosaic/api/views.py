b9f72ce2aa2a266ff3ffa21db42482c11ac8ff74
mildredosebe <mildredosebeochoki@gmail.com>
2025-06-24 10:12:33 +0300

    created orderlist api

---
+++ mosaic/api/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import viewsets
+from catalogue.models import Design
+from .serializers import DesignSerializer
+class DesignViewSet(viewsets.ModelViewSet):
+    queryset=Design.objects.all()
+    serializer_class=DesignSerializer
