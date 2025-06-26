bf4eee534e6cd0c28134b1b334d0c5fd124acedf
mildredosebe <mildredosebeochoki@gmail.com>
2025-06-26 09:33:08 +0300

    made changes on catalogue api

--- mosaic/api/urls.py
+++ mosaic/api/urls.py
@@ -1,15 +1,9 @@
 from django.urls import path ,include
 from rest_framework.routers import DefaultRouter
-from.views import (DesignViewSet,DesignersViewSet,TraderViewSet,OrderViewSet,PaymentViewSet,Shopping_cartViewSet,Transaction,design_review)
+from.views import DesignViewSet
 router=DefaultRouter()
 router.register(r"design",DesignViewSet,basename="design")
-router.register(r"designers",DesignViewSet,basename="designers")
-router.register(r"trader",DesignViewSet,basename="trader")
-router.register(r"order",DesignViewSet,basename="order")
-router.register(r"payment",DesignViewSet,basename="payment")
-router.register(r"shopping_cart",DesignViewSet,basename="shopping_cart")
-router.register(r"transaction",DesignViewSet,basename="transaction")
-router.register(r"design_review",DesignViewSet,basename="design_review")
+
 
 
 urlpatterns=[path("",include(router.urls)),]
