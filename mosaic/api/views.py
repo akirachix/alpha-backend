3d7ae702c575663d4b6e9e80839a419b7d07b4bc
cameronmcnz <you@example.com>
2025-06-26 15:12:05 +0300

    updated users code

--- mosaic/api/views.py
+++ mosaic/api/views.py
@@ -1,3 +1,17 @@
+from rest_framework import viewsets
+from django.shortcuts import render
+from users.models import Users
+
+
+from .serializers import UsersSerializer
+
+class UsersViewSet(viewsets.ModelViewSet):
+   queryset=Users.objects.all()
+   serializer_class=UsersSerializer
+
+
+
+
 
 
 
