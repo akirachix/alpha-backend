4e1867428249bcc424d9092a66fe60dbcebb9fbb
Marion <marionnyaisanga@gmail.com>
2025-06-27 14:35:25 +0300

    created an api for the payment app

--- mosaic/api/views.py
+++ mosaic/api/views.py
@@ -1,4 +1,13 @@
+from django.shortcuts import render
 
+
+# Create your views here.
+from rest_framework import viewsets
+from payment.models import Payment
+from .serializers import PaymentSerializer
+class PaymentViewSet(viewsets.ModelViewSet):
+   queryset=Payment.objects.all()
+   serializer_class=PaymentSerializer
 
 
 
