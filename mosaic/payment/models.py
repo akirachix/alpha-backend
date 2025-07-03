8b9c243ca5fb9fca835485bd59e24c936c093f41
Marion <marionnyaisanga@gmail.com>
2025-07-03 17:49:10 +0300

    created the payment api

--- mosaic/payment/models.py
+++ mosaic/payment/models.py
@@ -3,10 +3,9 @@ from order.models import Order
 from users.models import Users
 
 
-# Create your models here.
 class Payment(models.Model):
-#    order = models.ForeignKey(Order, on_delete=models.CASCADE)
-#    users = models.ForeignKey(Users, on_delete=models.CASCADE)
+   order = models.ForeignKey(Order, on_delete=models.CASCADE)
+   users = models.ForeignKey(Users, on_delete=models.CASCADE)
   amount = models.DecimalField(max_digits = 10, decimal_places = 2)
   phone_number = models.CharField()
   mpesa_receipt_number = models.CharField()
