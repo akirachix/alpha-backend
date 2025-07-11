452d3add67bf1c15a58afca977b551f1d1f29855
hani azanaw <haniazanawayelign@gmail.com>
2025-07-11 18:55:58 +0300

    Rename design_order to order_id and add migration

--- mosaic/order/models.py
+++ mosaic/order/models.py
@@ -1,14 +1,23 @@
 
+
 from django.db import models
 
+USER_TYPE_CHOICES = [
+    ('Designer', 'Designer'),
+    ('Trader', 'Trader'),
+]
 
 class Order(models.Model):
-    design_order = models.AutoField(primary_key=True)
+    order_id = models.AutoField(primary_key=True)
+    user_type = models.CharField(
+        max_length=20,
+        choices=USER_TYPE_CHOICES,
+        default='Trader'
+    )
     order_date = models.DateField()
     delivery_method = models.CharField(max_length=100)
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
     order_status = models.CharField(max_length=50)
 
     def __str__(self):
-        return f"Order {self.design_order}"
-
+        return f"Order {self.order_id}"
