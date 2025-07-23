4feff2bd2a798923403efabcabcd58df3547d84c
mildredosebe <mildredosebeochoki@gmail.com>
2025-07-23 18:58:04 +0300

    deleted repeated files

--- mosaic/design_review/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DesignReviewConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'design_review'
--- mosaic/shopping_cart/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Shopping_cart,Item
-
-admin.site.register(Shopping_cart)
-admin.site.register(Item)
